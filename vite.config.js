7535d795af86c729fe52729abf18bcf24459e11a
Dangelo Arrivillaga <dangeloarrivillaga@gmail.com>
2023-02-09 15:06:50 -0400

    4rd. commit: Update vite.config file 2pt. to make deploy

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/TheSmartProject3-0/',
+  base: '/Landing-Blockchain-Web3/',
   build: {
     rollupOptions: {
       output:{
